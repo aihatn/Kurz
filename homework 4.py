19dfb7b03e35621a00049ab6bdd9fea7dddb761a
aihatn <150720958+aihatn@users.noreply.github.com>
2024-08-14 15:39:29 +0200

    Add files via upload
    Kamil Chrastina

---
+++ homework 4.py
@@ -0,0 +1,21 @@
+from neopixel import NeoPixel
+from microbit import pin0
+from microbit import sleep
+
+def zapni(poradi_led):
+    nastav_barvu (poradi_led, (255, 255, 255))
+
+def vypni(poradi_led):
+    nastav_barvu (poradi_led, (0, 0, 0))
+
+def nastav_barvu(poradi_led, barva):
+    np[poradi_led] = barva
+    np.write()
+
+np = NeoPixel(pin0, 8)
+
+while True:
+    zapni(0)
+    sleep(1000)
+    vypni(0)
+    sleep(1000)
